507b0a00071a042c856066feef1812436af30078
ArthurSauerr <germanooarthuur@gmail.com>
2024-05-08 14:14:33 -0300

    Initial Commit

---
+++ Curiosity-app/Curiosity_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curiosity_appApp.swift
+//  Curiosity-app
+//
+//  Created by Lawson Beltrame on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Curiosity_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
